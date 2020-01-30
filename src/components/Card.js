47955b3f338c3ac780ac5e263727d348b2645574
DraGo EmPirE <banhawchun@gmail.com>
2020-01-30 14:33:09 +0700

    random robots

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Card = ({ id, name, email }) => {
+	let randomNum = Math.floor((Math.random() * 88) + 2);
 	return(
 		<div className="tc bg-light-green dib pa2 ma4 br3 grow bw2 shadow-5">
-			<img alt="Robots" src={`https://robohash.org/${id}?200x200`}/>
+			<img alt="Robots" src={`https://robohash.org/${randomNum}?200x200`}/>
 			<div>
 				<h2>{ name }</h2>
 				<p>{ email }</p>
--- src/containers/App.js
+++ src/containers/App.js
@@ -45,7 +45,6 @@ class App extends Component  {
 				</Scroll>
 			</div>
 			);
-		
 	}
 }
 
